c3ce9c5053fa367eaae1eb36c1c2ce3e0b027270
marclitchfield <marclitchfield@gmail.com>
2013-08-30 22:17:27 -0700

    Escape html on server

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
 var express = require('express'),
-	app = express(), 
-	http = require('http'),
-	server = http.createServer(app),
-	io = require('socket.io').listen(server);
+app = express(), 
+http = require('http'),
+server = http.createServer(app),
+io = require('socket.io').listen(server),
+escape = require('escape-html');
 
 app.use(express['static'](__dirname + '/public'));
 
 server.listen(80);
 
 io.sockets.on('connection', function (socket) {
-  socket.on('submitline', function (poemLine) {
-	  socket.emit('newline', poemLine);
-	  socket.broadcast.emit('newline', poemLine);
-  });
+	socket.on('submitline', function (poemLine) {
+		var escapedLine = escape(poemLine);	
+		socket.emit('newline', escapedLine);
+		socket.broadcast.emit('newline', escapedLine);
+	});
 });
