9aa71b345486d61c7889be2286e6b9d8aad951eb
David Rueck <drueck@gmail.com>
2013-09-07 23:26:09 -0700

    Connect poems routes to the database with PoemsRepository
    This is a super rough draft with no error handling and not much thought
    as to how we want to do things long term. Just wanted to get some basic
    database interaction going.

--- routes/poem.js
+++ routes/poem.js
@@ -1,28 +1,26 @@
 var _ = require('underscore');
 
-var poems = [
-  { id: 1, name: 'chon chon' },
-  { id: 2, name: 'cuddly chang' },
-  { id: 3, name: 'billy grippa' },
-  { id: 4, name: 'francis egor' }
-];
+var PoemsRepository = require('../lib/repositories/poems_repository.js');
+var poemsRepo = new PoemsRepository();
 
 exports.list = function(req, res) {
-  res.render('poem/list', { poems: poems });
+	poemsRepo.all(function(err, poems) {
+		res.render('poem/list', { poems: poems });
+	});
 }
 
 exports.edit = function(req, res) {
-  console.log('poems', poems);
-  var poem = _.find(poems, function(p) { return p.id == req.params.id; });
-  res.render('poem/edit', { poem: poem });
+	poemsRepo.read(req.params.id, function(err, poem) {
+		res.render('poem/edit', { poem: poem });
+	});
 }
 
 exports.createform = function(req, res) {
-  res.render('poem/new');
+	res.render('poem/new');
 }
 
 exports.create = function(req, res) {
-  var poem = { id: poems.length + 1, name: req.body.name };
-  poems.push(poem);
-  res.redirect('/poem/' + poem.id);
+	poemsRepo.create({ name: req.body.name }, function(err, poem) {
+		res.redirect('/poem/' + poem.id);
+	});
 }
