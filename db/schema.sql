226d170998baeac6411fa38b799e1e8267cb4db2
David Rueck <drueck@gmail.com>
2013-09-07 23:22:32 -0700

    Add database schema file and README about how to create the database

---
+++ db/schema.sql
@@ -0,0 +1,208 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: pocolab; Type: DATABASE; Schema: -; Owner: -
+--
+
+CREATE DATABASE pocolab WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'en_US.UTF-8' LC_CTYPE = 'en_US.UTF-8';
+
+
+\connect pocolab
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+
+--
+-- Name: plpgsql; Type: EXTENSION; Schema: -; Owner: -
+--
+
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+
+
+--
+-- Name: EXTENSION plpgsql; Type: COMMENT; Schema: -; Owner: -
+--
+
+COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
+
+
+SET search_path = public, pg_catalog;
+
+--
+-- Name: line_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE line_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+SET default_tablespace = '';
+
+SET default_with_oids = false;
+
+--
+-- Name: lines; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE lines (
+    id integer DEFAULT nextval('line_id_seq'::regclass) NOT NULL,
+    poem_id integer NOT NULL,
+    poet_id integer NOT NULL,
+    text character varying(200) NOT NULL
+);
+
+
+--
+-- Name: poem_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE poem_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: poems; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE poems (
+    id integer DEFAULT nextval('poem_id_seq'::regclass) NOT NULL,
+    name character varying(100)
+);
+
+
+--
+-- Name: poet_id_seq; Type: SEQUENCE; Schema: public; Owner: -
+--
+
+CREATE SEQUENCE poet_id_seq
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+--
+-- Name: poets; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE poets (
+    id integer DEFAULT nextval('poet_id_seq'::regclass) NOT NULL,
+    name character varying(50) NOT NULL,
+    email character varying(100) NOT NULL,
+    password character(64) NOT NULL
+);
+
+
+--
+-- Name: poets_poems; Type: TABLE; Schema: public; Owner: -; Tablespace: 
+--
+
+CREATE TABLE poets_poems (
+    poet_id integer NOT NULL,
+    poem_id integer NOT NULL
+);
+
+
+--
+-- Name: lines_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY lines
+    ADD CONSTRAINT lines_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: poems_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY poems
+    ADD CONSTRAINT poems_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: poets_email_key; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY poets
+    ADD CONSTRAINT poets_email_key UNIQUE (email);
+
+
+--
+-- Name: poets_name_key; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY poets
+    ADD CONSTRAINT poets_name_key UNIQUE (name);
+
+
+--
+-- Name: poets_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY poets
+    ADD CONSTRAINT poets_pkey PRIMARY KEY (id);
+
+
+--
+-- Name: poets_poems_pkey; Type: CONSTRAINT; Schema: public; Owner: -; Tablespace: 
+--
+
+ALTER TABLE ONLY poets_poems
+    ADD CONSTRAINT poets_poems_pkey PRIMARY KEY (poet_id, poem_id);
+
+
+--
+-- Name: lines_poem_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY lines
+    ADD CONSTRAINT lines_poem_id_fkey FOREIGN KEY (poem_id) REFERENCES poems(id);
+
+
+--
+-- Name: lines_poet_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY lines
+    ADD CONSTRAINT lines_poet_id_fkey FOREIGN KEY (poet_id) REFERENCES poets(id);
+
+
+--
+-- Name: poets_poems_poem_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY poets_poems
+    ADD CONSTRAINT poets_poems_poem_id_fkey FOREIGN KEY (poem_id) REFERENCES poems(id);
+
+
+--
+-- Name: poets_poems_poet_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: -
+--
+
+ALTER TABLE ONLY poets_poems
+    ADD CONSTRAINT poets_poems_poet_id_fkey FOREIGN KEY (poet_id) REFERENCES poets(id);
+
+--
+-- PostgreSQL database dump complete
+--
+
