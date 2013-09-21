6e3db5fbe923303863a7745485b6f78c903363d6
marclitchfield <marclitchfield@gmail.com>
2013-09-20 22:00:11 -0700

    webhook-deploy config and startup bash script
    Add the config file for webhook-deploy.
    Add a bash script to startup webhook deploy and the app running under
    node-supervisor. Creates a logs directory and directs log files there.

---
+++ deploy/startup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+SCRIPT_DIR=`dirname $0`
+echo $SCRIPT_DIR
+mkdir -p $SCRIPT_DIR/../logs
+sudo nohup supervisor $SCRIPT_DIR/../app.js > /dev/null 2> $SCRIPT_DIR/../logs/error.log &
+sudo nohup webhook-deployer -c $SCRIPT_DIR/webhook-deployer.config > $SCRIPT_DIR/../logs/deploy.log 2>&1 &
