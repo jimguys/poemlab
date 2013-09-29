006a6d8e76e7bf5f1f38a046a574b9a1927f899c
David Rueck <drueck@gmail.com>
2013-09-28 22:15:03 -0700

    Standardize on two spaces for indentation

--- server/repositories/poems_repository.js
+++ server/repositories/poems_repository.js
@@ -1,79 +1,79 @@
 module.exports = function(dbConfig) {
-	var db = require('./poemlab_database')(dbConfig);
+  var db = require('./poemlab_database')(dbConfig);
 
-	return {
+  return {
 
-		create: function(poem_data, callback) {
-			db.query("insert into poems (name) values ($1) returning id", [poem_data.name], function(err, result) {
-				if (err) { return callback(err); }
-				var poem = { id: result.rows[0].id, name: poem_data.name };
-				callback(null, poem);
-			});
-		},
+    create: function(poem_data, callback) {
+      db.query("insert into poems (name) values ($1) returning id", [poem_data.name], function(err, result) {
+        if (err) { return callback(err); }
+        var poem = { id: result.rows[0].id, name: poem_data.name };
+        callback(null, poem);
+      });
+    },
 
-		addPoet: function(poem_id, poet_id, callback) {
-			db.query("insert into poets_poems (poem_id, poet_id) values ($1, $2)", [poem_id, poet_id],
-				function(err, result) {
-					callback(err);
-				}
-			);
-		},
+    addPoet: function(poem_id, poet_id, callback) {
+      db.query("insert into poets_poems (poem_id, poet_id) values ($1, $2)", [poem_id, poet_id],
+        function(err, result) {
+          callback(err);
+        }
+      );
+    },
 
-		addPoets: function(poem_id, poet_ids, callback) {
-			db.query("insert into poets_poems (poem_id, poet_id) select $1, id from poets p " +
-				"where p.id = any($2::int[])", [poem_id, poet_ids],
-				function(err, result) {
-					callback(err);
-				}
-			);
-		},
+    addPoets: function(poem_id, poet_ids, callback) {
+      db.query("insert into poets_poems (poem_id, poet_id) select $1, id from poets p " +
+        "where p.id = any($2::int[])", [poem_id, poet_ids],
+        function(err, result) {
+          callback(err);
+        }
+      );
+    },
 
-		removePoet: function(poem_id, poet_id, callback) {
-			db.query("delete from poets_poems where poem_id = $1 and poet_id = $2", [poem_id, poet_id],
-				function(err, result) {
-					callback(err);
-				}
-			);
-		},
+    removePoet: function(poem_id, poet_id, callback) {
+      db.query("delete from poets_poems where poem_id = $1 and poet_id = $2", [poem_id, poet_id],
+        function(err, result) {
+          callback(err);
+        }
+      );
+    },
 
-		getPoets: function(poem_id, callback) {
-			db.query("select p.* from poets as p inner join poets_poems as pp " +
-				"on p.id = pp.poet_id where pp.poem_id = $1", [poem_id],
-				function(err, result) {
-					if(err) { return callback(err); }
-					callback(null, result.rows);
-				}
-			);
-		},
+    getPoets: function(poem_id, callback) {
+      db.query("select p.* from poets as p inner join poets_poems as pp " +
+        "on p.id = pp.poet_id where pp.poem_id = $1", [poem_id],
+        function(err, result) {
+          if(err) { return callback(err); }
+          callback(null, result.rows);
+        }
+      );
+    },
 
-		read: function(poem_id, callback) {
-			db.query("select * from poems where id = $1", [poem_id], function(err, result) {
-				if (err) { return callback(err); }
-				callback(null, result.rows[0]);
-			});
-		},
+    read: function(poem_id, callback) {
+      db.query("select * from poems where id = $1", [poem_id], function(err, result) {
+        if (err) { return callback(err); }
+        callback(null, result.rows[0]);
+      });
+    },
 
-		destroy: function(poem_id, callback) {
-			db.query("delete from poems where id = $1", [poem_id], function(err, result) {
-				callback(err);
-			});
-		},
+    destroy: function(poem_id, callback) {
+      db.query("delete from poems where id = $1", [poem_id], function(err, result) {
+        callback(err);
+      });
+    },
 
-		all: function(callback) {
-			db.query("select * from poems", [], function(err, result) {
-				if (err) { return callback(err); }
-				callback(null, result.rows);
-			});
-		},
+    all: function(callback) {
+      db.query("select * from poems", [], function(err, result) {
+        if (err) { return callback(err); }
+        callback(null, result.rows);
+      });
+    },
 
-		forPoet: function(poet_id, callback) {
-			db.query("select p.* from poems p inner join poets_poems as pp " +
-				"on p.id = pp.poem_id where pp.poet_id = $1", [poet_id],
-				function(err, result) {
-					if(err) { return callback(err); }
-					callback(null, result.rows);
-				}
-			);
-		}
-	};
+    forPoet: function(poet_id, callback) {
+      db.query("select p.* from poems p inner join poets_poems as pp " +
+        "on p.id = pp.poem_id where pp.poet_id = $1", [poet_id],
+        function(err, result) {
+          if(err) { return callback(err); }
+          callback(null, result.rows);
+        }
+      );
+    }
+  };
 };
