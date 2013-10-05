06642a90359433fd110b04b7ff213751a5c75585
marclitchfield <marclitchfield@gmail.com>
2013-10-04 22:38:29 -0700

    Show poets on edit screen, and assign a color to each.
    Poets are assigned a color from a palette. client-side logic applies the
    poet's color to an indicator in front of each line. Currently there is
    unsightly duplication of the DOM between the server and the client, so
    next step is to find a way to share DOM rendering logic between the client
    and the server.

--- public/js/poem/edit.js
+++ public/js/poem/edit.js
@@ -5,11 +5,34 @@ $(function() {
   var linesContainer = $('#lines');
   var lineTextInput = poemContainer.find('.line-text');
 
+  function assignColorToLine(lineElement) {
+    var poetId = $(lineElement).data('poet-id');
+    var poetColor = poemContainer.find('.poet[data-poet-id=' + poetId + ']').css('background-color');
+    $(lineElement).find('.box').css('background-color', poetColor);
+  }
+
+  (function assignColorsToPoets() {
+    var poetColors = [
+      '#8d91b0','#8db099','#abb08d','#b08f8d','#b08daa',
+      '#8d8db0','#8da6b0','#8db095','#aab08d'];
+    var poets = poemContainer.find('.poet').each(function(i, el) {
+      $(el).css('background-color', poetColors[i % poetColors.length]);
+    });
+    linesContainer.find('.line').each(function(i, el) {
+      assignColorToLine(el);
+    });
+  })();
+
   socket.on('line-created-for-poem-' + poemId, function(poemLine) {
-    $('<div/>', {
-      'class': 'line',
+    var lineDiv = $('<div class="line" data-poet-id="' +
+      poemLine.poetId + '"><div class="box"/></div>');
+    $('<span/>', {
       'text': poemLine.text
-    }).appendTo(linesContainer);
+    }).appendTo(lineDiv);
+
+    lineDiv.appendTo(linesContainer);
+
+    assignColorToLine(lineDiv);
     $(document).scrollTop($(document).height());
     lineTextInput.focus();
   });
