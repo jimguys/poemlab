2c6f78c87b27f61ab10d32d2cef7e694dbe40a70
David Rueck <drueck@gmail.com>
2013-10-04 22:34:50 -0700

    Add middleware to redirect based on logged in or not and route

---
+++ server/routes/middleware.js
@@ -0,0 +1,29 @@
+var signedOutOnlyRoutes = ['/', '/login', '/register'];
+
+function requestingSignedOutRoute(req) {
+  return signedOutOnlyRoutes.indexOf(req.url) !== -1;
+}
+
+function requestingSignedInRoute(req) {
+  return !requestingSignedOutRoute(req) && req.url !== '/login';
+}
+
+function signedIn(req) {
+  return !!req.user;
+}
+
+function signedOut(req) {
+  return !signedIn(req);
+}
+
+module.exports = {
+  redirectBasedOnLoggedInStatus: function(req, res, next) {
+    if (signedIn(req) && requestingSignedOutRoute(req)) {
+      res.redirect('/poem');
+    } else if (signedOut(req) && requestingSignedInRoute(req)) {
+      res.redirect('/');
+    } else {
+      next();
+    }
+  }
+};
