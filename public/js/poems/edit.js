062bd1687a5dc471c7fb4e213f43cfbc4a5b80ae
Marc Litchfield <marclitchfield@gmail.com>
2013-10-18 22:46:44 -0700

    Render poet colors on the server to eliminate flicker.
    There are some tricky bits to this implementation. There is a poet-color partial that contains a function to compute the poet color css class from the poetId. The client needs to provide the partial with a list of poet objects, but it just creates object literals with a poet id property, because that is all that is required by the poetColorClass function.  Would be nice to find a way to avoid having to do this.

--- public/js/poems/edit.js
+++ public/js/poems/edit.js
@@ -9,28 +9,12 @@ $(function() {
     socket.emit('joinPoem', poemId);
   });
 
-  function assignColorToLine(lineElement) {
-    var poetId = $(lineElement).data('poet-id');
-    var poetColor = poemContainer.find('.poet[data-poet-id=' + poetId + ']').css('background-color');
-    $(lineElement).find('.box').css('background-color', poetColor);
-  }
-
-  (function assignColorsToPoets() {
-    var poetColors = [
-      '#a8aabd','#8db099','#abb08d','#b08f8d','#b08daa',
-      '#8d8db0','#8da6b0','#8db095','#aab08d'];
-    var poets = poemContainer.find('.poet').each(function(i, el) {
-      $(el).css('background-color', poetColors[i % poetColors.length]);
-    });
-    linesContainer.find('.line').each(function(i, el) {
-      assignColorToLine(el);
-    });
-  })();
-
   socket.on('line-created-for-poem-' + poemId, function(poemLine) {
-    var lineDiv = $(jade.render('server/views/partials/line.jade', { line: poemLine }));
+    var lineDiv = $(jade.render('server/views/partials/line.jade', {
+      line: poemLine,
+      poets: $('.poet').map(function(i, poet) { return { id: $(poet).data('poet-id') }; })
+    }));
     lineDiv.appendTo(linesContainer);
-    assignColorToLine(lineDiv);
     $(document).scrollTop($(document).height());
     lineTextInput.focus();
   });
